b39acd5a92fc8e2aadf41a70d9241452340b6708
HarinezumiSama <vitalii.maklai@gmail.com>
2018-10-24 09:07:40 -0400

    Game: * Better argument checks * Handling special case for Queen of Spades * Minor changes * Improved/fixed unit tests

--- src/HarinezumiSama.SimpleBridge125.UnitTests/GameTests.cs
+++ src/HarinezumiSama.SimpleBridge125.UnitTests/GameTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace HarinezumiSama.SimpleBridge125.UnitTests
 {
@@ -6,15 +7,29 @@ namespace HarinezumiSama.SimpleBridge125.UnitTests
     internal sealed class GameTests
     {
         [Test]
-        public void TestConstruction()
+        [TestCase(new object[] { new[] { "John Doe", "Jill Doe" } })]
+        public void TestConstructionWithValidArgument(string[] playerNames)
         {
-            var game = new Game(new[] { "John Doe", "Jill Doe" });
+            var game = new Game(playerNames);
 
             Assert.That(game.Players, Is.Not.Null);
-            Assert.That(game.Players.Count, Is.EqualTo(2));
+            Assert.That(game.Players.Count, Is.EqualTo(playerNames.Length));
             Assert.That(game.CurrentDealerIndex, Is.EqualTo(0));
             Assert.That(game.PointsRatio, Is.EqualTo(1));
-            Assert.That(game.ActiveStack.Cards.Count, Is.EqualTo(1));
+            Assert.That(game.ActiveStack.Cards.Count, Is.EqualTo(0));
+            Assert.That(game.RequiredFirstCard, Is.Not.Null);
+        }
+
+        [Test]
+        public void TestConstructionWithInvalidArgument()
+        {
+            //// ReSharper disable once AssignNullToNotNullAttribute - Negative test case
+            Assert.That(() => new Game(null), Throws.ArgumentNullException);
+
+            Assert.That(() => new Game(new string[0]), Throws.TypeOf<ArgumentOutOfRangeException>());
+            Assert.That(() => new Game(new[] { "One player" }), Throws.TypeOf<ArgumentOutOfRangeException>());
+            Assert.That(() => new Game(new[] { "One player", null }), Throws.ArgumentException);
+            Assert.That(() => new Game(new[] { "One player", string.Empty }), Throws.ArgumentException);
         }
     }
 }
