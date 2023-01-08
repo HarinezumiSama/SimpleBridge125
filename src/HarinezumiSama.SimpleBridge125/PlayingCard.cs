06c2f8eef09b1f5b75fd3b732d5ae59a023aa806
Vitalii Maklai <vitalii.maklai@gmail.com>
2023-01-07 20:40:13 -0500

    Migration: Phase 2

--- src/HarinezumiSama.SimpleBridge125/PlayingCard.cs
+++ src/HarinezumiSama.SimpleBridge125/PlayingCard.cs
@@ -4,31 +4,9 @@ using System.Diagnostics;
 namespace HarinezumiSama.SimpleBridge125
 {
     [DebuggerDisplay(@"{ToDebuggerString(),nq}")]
-    public readonly struct PlayingCard : IEquatable<PlayingCard>
+    public readonly record struct PlayingCard(PlayingCardRank Rank, PlayingCardSuit Suit)
     {
-        public PlayingCard(PlayingCardRank rank, PlayingCardSuit suit)
-        {
-            Rank = rank;
-            Suit = suit;
-        }
-
-        public PlayingCardRank Rank { get; }
-
-        public PlayingCardSuit Suit { get; }
-
         public override string ToString() => $@"{Rank.AsString()}{Suit.AsString()}";
-
-        public override bool Equals(object obj) => obj is PlayingCard other && Equals(other);
-
-        public override int GetHashCode() => Rank.CombineHashCodes(Suit);
-
-        public bool Equals(PlayingCard other) => Equals(this, other);
-
-        public static bool Equals(PlayingCard left, PlayingCard right) => left.Rank == right.Rank && left.Suit == right.Suit;
-
-        public static bool operator ==(PlayingCard left, PlayingCard right) => Equals(left, right);
-
-        public static bool operator !=(PlayingCard left, PlayingCard right) => !Equals(left, right);
 
         private string ToDebuggerString() => $@"{GetType().GetQualifiedName()}: {ToString()}";
     }
